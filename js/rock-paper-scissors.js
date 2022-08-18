df096a2a469df4c1c88400f758b8d553e9974b9d
Kenya Chan <kenya.chan@gmail.com>
2022-08-18 23:18:19 +1000

    Add playRound function

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -9,3 +9,41 @@ function getComputerChoice() {
     // 	return array item corresponding to random number
     return CHOICE[randomNumber];
 }
+
+function playRound(playerSelection, computerSelection) {
+    let ps = playerSelection.toLowerCase();
+    let cs = computerSelection.toLowerCase();
+
+    if (ps === cs) {
+        return `It's a tie! You've both picked '${ps}'`;
+    }
+
+    if (ps === 'rock' && cs === 'scissors') {
+        return 'You Win! Rock beats Scissors';
+    }
+
+    if (ps === 'paper' && cs === 'rock') {
+        return 'You Win! Paper beats Rock';
+    }
+
+    if (ps === 'scissors' && cs === 'paper') {
+        return 'You Win! Scissors beats Paper';
+    }
+
+    if (ps === 'rock' && cs === 'paper') {
+        return 'You Lose! Paper beats Rock';
+    }
+
+    if (ps === 'paper' && cs === 'scissors') {
+        return 'You Lose! Scissors beats Paper';
+    }
+
+    if (ps === 'scissors' && cs === 'rock') {
+        return 'You Lose! Rock beats Scissors';
+    }
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
