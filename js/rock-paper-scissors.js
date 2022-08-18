550487dba2dce275066b177cb1572f71436b8766
Kenya Chan <kenya.chan@gmail.com>
2022-08-18 22:57:25 +1000

    Refactor

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -3,11 +3,8 @@ const CHOICE = ['rock', 'paper', 'scissors'];
 
 // FUNCTION Get Computer Choice
 function getComputerChoice() {
-    // 	Initialise a variable called randomNumber
-    let randomNumber;
-    
-    // 	Generate a random number between 0 and 2 and save it to randomNumber
-    randomNumber = Math.floor(Math.random() * 3);
+    // 	Generate a random number between 0 and 2 and save it to a variable called randomNumber
+    let randomNumber = Math.floor(Math.random() * 3);
 
     // 	return array item corresponding to random number
     return CHOICE[randomNumber];
