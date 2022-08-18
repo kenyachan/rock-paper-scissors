92e17ef3eb5a77aadb81f0c0b55378a6d9273f40
Kenya Chan <kenya.chan@gmail.com>
2022-08-18 22:40:14 +1000

    Add pseudocode for getComputerChoice function

---
+++ js/rock-paper-scissors.js
@@ -0,0 +1,8 @@
+// Array (rock, paper, scissors)
+// FUNCTION Get Computer Choice
+// 	Initialise a variable called choice
+// 	Initialise a variable called randomNumber
+// 	Generate a random number between 1 and 3 and save it to randomNumber
+// 	return array item corresponding to random number
+// END FUNCTION
+//
